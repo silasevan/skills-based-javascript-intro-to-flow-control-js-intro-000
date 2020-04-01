efea99682357616789b8bd78b4954298d0ae5aa9
ivang <silasevan@gmail.com>
2020-04-01 23:40:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,14 +14,46 @@ function teenager(age) {
 
 }
 
-function ageChecker(age) {
+function ageChecker(age){
+    if ((age>=13) && (age<=19)){
+        return "you are a teenager";
+    }
+    else if(age<=12){
+        return "you are a kid";
+    }
+    else{
+        return "you are a grownup";
+    }
+}
 
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age){
+    return ((age>=13)&&(age<=19))? "you are a teenager":
+    "you are not a teenager";
+}
 
 }
 
-function switchAge(age) {
+function switchAge(age){
+    switch((age>=13) &&(age<=19)){
+            case(13):
+            return "teenager";
+            break;
+            case(14):
+            return "teenager";
+            break;case(15):
+            return "teenager";
+            break; case(17):
+            return "teenager";
+            break; case(18):
+            return "teenager";
+            break; case(19):
+            return "teenager";
+            break;
+        default:
+    return "not among age group";
+}
+}
 
 }
